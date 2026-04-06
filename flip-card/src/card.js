40d5b849cdbc11bc38e92bcc5c42933583f99e02
Sreynich <srey45224@gmail.com>
2026-04-06 10:31:27 +0700

    add img and card.js

--- flip-card/src/card.js
+++ flip-card/src/card.js
@@ -1,3 +1,38 @@
-const cardImg = [
-   { "src": "./img/" }
-]
+import card1 from "./assets/card1.jpg";
+import card2 from "./assets/card2.jpg";
+import card3 from "./assets/card3.jpg";
+import card4 from "./assets/card4.jpg";
+import fruit1 from "./assets/fruit1.jpg";
+import fruit2 from "./assets/fruit2.jpg";
+import fruit3 from "./assets/fruit3.jpg";   
+import fruit4 from "./assets/fruit4.jpg";
+import animal1 from "./assets/animal1.jpg";
+import animal2 from "./assets/animal2.jpg"; 
+import animal3 from "./assets/animal3.jpg";
+import animal4 from "./assets/animal4.jpg";
+import flower1 from "./assets/flower1.jpg";
+import flower2 from "./assets/flower2.jpg";
+import flower3 from "./assets/flower3.jpg";
+import flower4 from "./assets/flower4.jpg";
+
+export const cardImg = [
+   { "src": "./assets/card1.jpg" },
+   { "src": "./assets/card2.jpg" },
+   { "src": "./assets/card3.jpg" },
+   { "src": "./assets/card4.jpg" },
+
+   { "src": "./assets/fruit1.jpg" },
+   { "src": "./assets/fruit2.jpg" },
+   { "src": "./assets/fruit3.jpg" },
+   { "src": "./assets/fruit4.jpg" },
+
+   { "src": "./assets/animal1.jpg" },
+   { "src": "./assets/animal2.jpg" },
+   { "src": "./assets/animal3.jpg" },
+   { "src": "./assets/animal4.jpg" },
+
+   { "src": "./assets/flower1.jpg" },
+   { "src": "./assets/flower2.jpg" },
+   { "src": "./assets/flower3.jpg" },
+   { "src": "./assets/flower4.jpg" },
+];
